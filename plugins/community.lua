2698b348829fc3c72774b61a4d0db384cbafa21d
Zhao Wei Liew <zhaoweiliew@gmail.com>
2023-07-16 20:37:49 +0800

    configure kanagawa theme, copilot, and typescript

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,4 +6,7 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.pack.typescript-all-in-one" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.colorscheme.kanagawa-nvim" },
 }
