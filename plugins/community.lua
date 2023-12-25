724c34f3837e61e33421e06e2a647834e2447d43
Zhao Wei Liew <zhaoweiliew@gmail.com>
2023-12-25 19:33:37 +0800

    add rust pack

--- plugins/community.lua
+++ plugins/community.lua
@@ -23,6 +23,7 @@ return {
   { import = "astrocommunity.pack.html-css" },
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.python-ruff" },
+  { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.typescript-all-in-one" },
   { import = "astrocommunity.pack.typst" },
 
