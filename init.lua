33efab81023b36c09133b4cdd2ea0d38e45677ae
Zhao Wei Liew <zhaoweiliew@gmail.com>
2024-07-31 02:51:47 +0800

    Add tree-sitter integration for ry

--- init.lua
+++ init.lua
@@ -17,3 +17,20 @@ end
 
 require "lazy_setup"
 require "polish"
+
+vim.filetype.add {
+  extension = {
+    ry = "ry",
+  },
+}
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+parser_config.ry = {
+  install_info = {
+    url = "~/_me/gh/riscy-business/tree-sitter",
+    files = { "src/parser.c" }, -- note that some parsers also require src/scanner.c or src/scanner.cc
+    -- optional entries:
+    branch = "main", -- default branch in case of git repo if different from master
+    generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+    requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+  },
+}
