3aa3849f37bc2b1bb5798a60c819a85ece206d24
Zhao Wei Liew <zhaoweiliew@gmail.com>
2024-08-04 00:08:23 +0800

    Add tree-sitter-c3

--- init.lua
+++ init.lua
@@ -27,10 +27,23 @@ local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
 parser_config.ry = {
   install_info = {
     url = "~/_me/gh/riscy-business/tree-sitter",
-    files = { "src/parser.c" }, -- note that some parsers also require src/scanner.c or src/scanner.cc
-    -- optional entries:
-    branch = "main", -- default branch in case of git repo if different from master
-    generate_requires_npm = false, -- if stand-alone parser without npm dependencies
-    requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+    files = { "src/parser.c" },
+    branch = "main",
+  },
+}
+
+vim.filetype.add {
+  extension = {
+    c3 = "c3",
+    c3i = "c3",
+    c3t = "c3",
+  },
+}
+
+parser_config.c3 = {
+  install_info = {
+    url = "https://github.com/c3lang/tree-sitter-c3",
+    files = { "src/parser.c", "src/scanner.c" },
+    branch = "main",
   },
 }
